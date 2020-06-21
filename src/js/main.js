097fede2baa706539cfe6d31f576654e7ec3ed8f
AlLosevK <al.losev.k@gmail.com>
2020-06-21 16:14:37 +0300

    Login+Homepage

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,21 @@ new Glide('.glide', {
   type: 'carousel',
   startAt: 0,
   perView: 4,
+  perTouch: 1,
+  breakpoints: {
+    768: {
+      perView: 2
+    },
+    600: {
+      perView: 1,
+      focusAt: 'center',
+      perTouch: 1
+    }
+  },
   focusAt: 'center',
   gap: 32
 }).mount();
+
 
 var menuBTN = document.getElementById('menuBTN');
 var menu = document.getElementById('menuNav'); // or whatever triggers the toggle
